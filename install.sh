36345c326ebaa7293840391da50d9e2bc3548b69
Francois Clad <fclad@unistra.fr>
2012-10-22 09:36:08 +0200

    First commit

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Retrieve path to the current directory
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+# Create a symbolic link to this folder from $HOME/.tmux
+if [ "${DIR}" != "${HOME}/.tmux" ]; then
+	if [ -e "${HOME}/.tmux" ]; then
+		echo "Warning: a .tmux folder already exists. Old folder moved to tmux_bak."
+		mv ${HOME}/.tmux ${HOME}/tmux_bak
+	fi
+	echo "Creating symbolic link for .tmux folder."
+	ln -s ${DIR} ${HOME}/.tmux
+fi
+
+# Create a symbolic link to tmux.conf file from $HOME/.tmux.conf
+if [ ! -L "${HOME}/.tmux.conf" ]; then
+	if [ -e "${HOME}/.tmux.conf" ] ; then
+		echo "Warning: a .tmux.conf file already exists. Old file moved to tmux.conf_bak."
+		mv ${HOME}/.tmux.conf ${HOME}/tmux.conf_bak 
+	fi
+	echo "Creating symbolic link for .tmux.conf file."
+	ln -s ${DIR}/tmux.conf ${HOME}/.tmux.conf
+fi
+
---
+++ layouts/cdev.tmux
@@ -0,0 +1,10 @@
+# Layout file for C devel
+
+selectp -t 0
+splitw -v -p 20
+selectp -t 0
+splitw -h -p 25
+selectp -t 2
+splitw -h -p 50
+selectp -t 0
+
